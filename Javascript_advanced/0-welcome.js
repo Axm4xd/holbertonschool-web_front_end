2ba82e72bed9704e430d7c3ae8f4bf7604114a0c
Axm4xd <axmedquliyev68@gmail.com>
2025-09-29 14:44:36 +0400

    fixed

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,8 @@
+function Welcome(firstname, lastname) {
+    let fullname=firstname+" "+lastname;
+
+    function displayFullname() {
+        alert("Welcome"+fullname+"!");
+    }
+    displayFullname();
+}
